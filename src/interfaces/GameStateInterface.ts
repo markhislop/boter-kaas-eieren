6510483f1039d79b4367fd26b77be44675bc4acf
Mark Hislop <mark-hislop@outlook.com>
2024-01-05 10:50:35 +0100

    Updates

--- src/interfaces/GameStateInterface.ts
+++
@@ -1,7 +0,0 @@
-export interface GameStateInterface {
-    activePlayer: "player_one" | "player_two"
-    fields: Array<{
-        value: string,
-        color: string
-    }>,
-}
--- src/interfaces/PlayerInterface.ts
+++
@@ -1,9 +0,0 @@
-export interface PlayerInterface {
-    [key: string]: {
-      id: "player_one" | "player_two"
-      title: string,
-      score: number,
-      marking: string,
-      color: string
-    }
-}
